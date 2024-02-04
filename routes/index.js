54772f5fb8c2bc955a1efb4facc1fd21c81c2fe9
Mahdi Mortuza <rah@localhost.localdomain>
2024-02-04 19:11:41 +0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import moduleRouter from "./module.js";
+import quizRouter from "./quiz.js";
+const router = Router();
+
+router.use("/module", moduleRouter);
+router.use("/quiz", quizRouter);
+
+
+export default router;
